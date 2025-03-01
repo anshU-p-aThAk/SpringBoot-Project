31cdd5963238d93da09211e7dcd885700cb67e9e
Anshu Pathak <surajshiv15ss@gmail.com>
2025-03-01 22:50:17 +0530

    first

---
+++ SpringBoot-Project/intro/src/main/java/com/first/intro/controller/MyController.java
@@ -0,0 +1,21 @@
+package com.first.intro.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String printHello(){
+        return "Hello To Capgemini";
+    }
+
+    @GetMapping("/hello")
+    public String displayHello(){
+        return "hello";
+    }
+}
