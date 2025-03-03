268eb71eabd0a501808ee44fc40bcc9acee6a70b
Anshu Pathak <surajshiv15ss@gmail.com>
2025-03-03 14:58:24 +0530

    uc4 done

---
+++ src/main/java/com/spring/project/controller/MessagingAppController.java
@@ -0,0 +1,27 @@
+package com.spring.project.controller;
+
+import com.spring.project.dto.UserDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MessagingAppController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from BridgeLabz!";
+    }
+
+    @GetMapping("/hello/query")
+    public String hello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+
+    @GetMapping("/hello/param/{name}")
+    public String helloParam(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+
+    @PostMapping("/hello/post")
+    public String hello(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz!";
+    }
+}
