3cea32c71f3756839fe6fdd76ecf3f91d2cb794f
Anshu Pathak <surajshiv15ss@gmail.com>
2025-03-03 14:54:20 +0530

    some changes done in uc2

---
+++ src/main/java/com/spring/project/controller/MessagingAppController.java
@@ -0,0 +1,19 @@
+package com.spring.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessagingAppController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from BridgeLabz!";
+    }
+
+    @GetMapping("/hello/query")
+    public String hello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+}
