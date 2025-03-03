dfe6d5b85f9e3b3f74ba6f5f5bb6c053d9df2d15
Anshu Pathak <surajshiv15ss@gmail.com>
2025-03-03 14:56:01 +0530

    some changes in uc3

---
+++ src/main/java/com/spring/project/controller/MessagingAppController.java
@@ -0,0 +1,24 @@
+package com.spring.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessagingAppController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from BridgeLabz!";
+    }
+
+    @GetMapping("/hello/query")
+    public String hello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+
+    @GetMapping("/hello/param/{name}")
+    public String helloParam(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+}
